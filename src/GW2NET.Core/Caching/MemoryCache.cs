5ced2075b4fed247f0bc7a2504668afdf52030c1
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2016-02-07 00:21:45 +0100

    Add cache interface and memory cache implementation

---
+++ src/GW2NET.Core/Caching/MemoryCache.cs
@@ -0,0 +1,85 @@
+﻿// <copyright file="MemoryCache.cs" company="GW2.NET Coding Team">
+// This product is licensed under the GNU General Public License version 2 (GPLv2). See the License in the project root folder or the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+
+namespace GW2NET.V2.Colors.newStuff
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    using GW2NET.Common;
+
+    public class MemoryCache<T> : ICache<T>
+    {
+        private readonly SortedSet<T> items;
+
+        private MemoryCache(SortedSet<T> items)
+        {
+            this.items = items;
+        }
+
+        public IEnumerable<T> Value
+        {
+            get
+            {
+                return this.items.Where(this.CheckIfNotStale);
+            }
+        }
+
+        public static MemoryCache<T> Default()
+        {
+            return new MemoryCache<T>(new SortedSet<T>());
+        }
+
+        public void Add(T item)
+        {
+            this.items.Add(item);
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                this.Add(item);
+            }
+        }
+
+        public void Clear()
+        {
+            this.items.Clear();
+        }
+
+        public IEnumerable<T> Get(Func<T, bool> selector, CultureInfo culture)
+        {
+            foreach (T item in this.items)
+            {
+                if (selector(item) && this.CheckIfNotStale(item))
+                {
+                    var localizedItem = item as ILocalizable;
+                    if (localizedItem == null)
+                    {
+                        yield return item;
+                    }
+                    else
+                    {
+                        yield return Equals(localizedItem.Culture, culture) ? item : default(T);
+                    }
+                }
+            }
+        }
+
+        private bool CheckIfNotStale(T item)
+        {
+            ITimeSensitive timesensitiveItem = item as ITimeSensitive;
+
+            if (timesensitiveItem == null)
+            {
+                return true;
+            }
+
+            return timesensitiveItem.Expires < DateTimeOffset.UtcNow;
+        }
+    }
+}
---
+++ src/GW2NET.V2.Colors.Tests/Foo.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="Foo.cs" company="GW2.NET Coding Team">
+// This product is licensed under the GNU General Public License version 2 (GPLv2). See the License in the project root folder or the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+
+namespace GW2NET
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+
+    using GW2NET.Colors;
+    using GW2NET.Common.Converters;
+    using GW2NET.Common.Serializers;
+    using GW2NET.Compression;
+    using GW2NET.V2.Colors.Converters;
+    using GW2NET.V2.Colors.newStuff;
+
+    using Xunit;
+
+    public class Foo
+    {
+        [Fact]
+        public async void DiscoverTest()
+        {
+            HttpClient client = new HttpClient(new GW2ApiHandler(), false)
+            {
+                BaseAddress = new Uri("https://api.guildwars2.com/")
+            };
+
+            JsonSerializerFactory jsonSerializerFactory = new JsonSerializerFactory();
+            GzipInflator gzipInflator = new GzipInflator();
+
+            HttpResponseConverter httpResponseConverter = new HttpResponseConverter(jsonSerializerFactory, gzipInflator);
+
+            ConverterAdapter<IEnumerable<int>> idConverter = new ConverterAdapter<IEnumerable<int>>();
+            var colorConverter = new ColorPaletteConverter(new ColorConverter(), new ColorModelConverter(new ColorConverter()));
+
+            ColorService service = new ColorService(client, httpResponseConverter, MemoryCache<ColorPalette>.Default(), idConverter, colorConverter);
+
+            IEnumerable<int> ids = await service.DiscoverAsync();
+            
+            Assert.NotEmpty(ids);
+        }
+    }
+}
