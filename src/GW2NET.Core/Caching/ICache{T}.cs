47b8905ebb2e105df036a030da6cb35d28ae8b31
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2016-02-07 00:21:45 +0100

    Add cache interface and memory cache implementation

---
+++ src/GW2NET.Core/Caching/ICache{T}.cs
@@ -0,0 +1,19 @@
+﻿namespace GW2NET.V2.Colors.newStuff
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    public interface ICache<T>
+    {
+        IEnumerable<T> Value { get; }
+
+        void Add(T item);
+
+        void AddRange(IEnumerable<T> items);
+
+        void Clear();
+
+        IEnumerable<T> Get(Func<T, bool> selector, CultureInfo culture);
+    }
+}
