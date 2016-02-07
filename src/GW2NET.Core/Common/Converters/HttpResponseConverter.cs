033c204cda3db3cb6cc0e4da31bb67a6c7a479ee
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2016-02-07 19:51:02 +0100

    Update HttpResponseConverter to support new repository layout
    With this commit the user only needs to pass a converter which convertas a single item into the repository. Set conversion is done in parallel by the HttpResponseConverter itself.

--- src/GW2NET.Core/Common/Converters/HttpResponseConverter.cs
+++ src/GW2NET.Core/Common/Converters/HttpResponseConverter.cs
@@ -1,7 +1,9 @@
 namespace GW2NET.Common.Converters
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.IO;
     using System.Linq;
     using System.Net.Http;
@@ -22,44 +24,83 @@ namespace GW2NET.Common.Converters
             this.gzipInflator = gzipInflator;
         }
 
-        public async Task<TOutput> ConvertAsync<TInput, TOutput>(HttpResponseMessage responseMessage, IConverter<TInput, TOutput> innerConverter, CancellationToken cancellationToken, object state = null)
+        public async Task<IEnumerable<TOutput>> ConvertCollectionAsync<TInput, TOutput>(HttpResponseMessage responseMessage, IConverter<TInput, TOutput> innerConverter, CancellationToken cancellationToken, object state = null)
         {
-            if (responseMessage == null)
+            var contentStream = await this.GetContent(responseMessage, state);
+
+            IEnumerable<TInput> response = this.serializerFactory.GetSerializer<IEnumerable<TInput>>().Deserialize(contentStream);
+
+            ConcurrentBag<TOutput> items = new ConcurrentBag<TOutput>();
+
+            //ToDo: Implement proper
+            var responseState = new Response<TInput>
             {
-                throw new ArgumentNullException(nameof(responseMessage));
+                Culture = CultureInfo.CurrentCulture,
+                Date = new DateTimeOffset(DateTime.Now)
+            };
+
+            Parallel.ForEach(response, item =>
+            {
+                items.Add(innerConverter.Convert(item, responseState));
+            });
+
+            return items;
+        }
+
+        public async Task<TOutput> ConvertElementAsync<TInput, TOutput>(HttpResponseMessage responseMessage, IConverter<TInput, TOutput> innerConverter, CancellationToken cancellationToken, object state = null)
+        {
+            var contentStream = await this.GetContent(responseMessage, state);
+
+            TInput response = this.serializerFactory.GetSerializer<TInput>().Deserialize(contentStream);
+
+            //ToDo: Implement proper
+            var responseState = new Response<TInput>
+            {
+                Culture = CultureInfo.CurrentCulture,
+                Date = new DateTimeOffset(DateTime.Now)
+            };
+
+            return innerConverter.Convert(response, responseState);
+        }
+
+        private async Task<Stream> GetContent(HttpResponseMessage message, object state = null)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
             }
 
-            using (HttpContent content = responseMessage.Content)
+            using (HttpContent content = message.Content)
             {
-                Stream contentStream = await content.ReadAsStreamAsync();
-                if (contentStream == null)
+                byte[] buffer = new byte[4096];
+
+                Stream contentStream = new MemoryStream(buffer, true);
+
+                await content.CopyToAsync(contentStream);
+
+                if (contentStream.Length == 0)
                 {
-                    throw new ServiceException("The content stream is null.");
+                    throw new ServiceException("Could not read content stream.");
                 }
 
-                // ToDo: Needs to be done better!
-                IEnumerable<string> encoding;
-                if (responseMessage.Headers.TryGetValues("Content-Encoding", out encoding))
+                var encoding = message.Headers.SingleOrDefault(h => h.Key == "Content-Encoding").Value;
+                if (encoding != null)
                 {
-                    List<string> encodingList = encoding.ToList();
-
-                    if (encodingList.FirstOrDefault() != null)
+                    if (encoding.FirstOrDefault().Equals("gzip", StringComparison.OrdinalIgnoreCase))
                     {
-                        bool compressed = encodingList.First().Equals("gzip", StringComparison.OrdinalIgnoreCase);
-                        if (compressed)
+                        Stream uncompressed = this.gzipInflator.Convert(contentStream, state);
+                        if (uncompressed == null)
                         {
-                            Stream uncompressed = this.gzipInflator.Convert(contentStream, state);
-                            if (uncompressed != null)
-                            {
-                                contentStream = uncompressed;
-                            }
+                            throw new InvalidOperationException("Could not read stream.");
                         }
+
+                        contentStream = uncompressed;
                     }
                 }
 
-                TInput responseContent = this.serializerFactory.GetSerializer<TInput>().Deserialize(contentStream);
-
-                return innerConverter.Convert(responseContent, state);
+                await contentStream.FlushAsync();
+                contentStream.Position = 0;
+                return contentStream;
             }
         }
     }
