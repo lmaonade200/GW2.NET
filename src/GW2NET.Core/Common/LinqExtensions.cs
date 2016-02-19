3198e8a3b246ff073bb6e0da9c037eaece9e0e53
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2016-02-19 22:25:58 +0100

    Add SymmetricExcept extension method

---
+++ src/GW2NET.Core/Common/LinqExtensions.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="LinqExtensions.cs" company="GW2.NET Coding Team">
+// This product is licensed under the GNU General Public License version 2 (GPLv2). See the License in the project root folder or the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+
+namespace GW2NET.Common
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> SymmetricExcept<T>(this IEnumerable<T> seq1, IEnumerable<T> seq2)
+        {
+            HashSet<T> hashSet = new HashSet<T>(seq1);
+            hashSet.SymmetricExceptWith(seq2);
+            return hashSet.Select(x => x);
+        }
+    }
+}
