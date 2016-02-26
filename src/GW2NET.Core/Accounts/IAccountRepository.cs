0daf3df8c82ea863efbead7bf80dea47366d1ba4
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2016-02-26 23:08:42 +0100

    Remove synchrnonous account request

--- src/GW2NET.Core/Accounts/IAccountRepository.cs
+++ src/GW2NET.Core/Accounts/IAccountRepository.cs
@@ -1,11 +1,6 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="IAccountRepository.cs" company="GW2.NET Coding Team">
-//   This product is licensed under the GNU General Public License version 2 (GPLv2). See the License in the project root folder or the following page: http://www.gnu.org/licenses/gpl-2.0.html
+﻿// <copyright file="IAccountRepository.cs" company="GW2.NET Coding Team">
+// This product is licensed under the GNU General Public License version 2 (GPLv2). See the License in the project root folder or the following page: http://www.gnu.org/licenses/gpl-2.0.html
 // </copyright>
-// <summary>
-//   Provides the interface for repositories that provide player data.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
 
 namespace GW2NET.Accounts
 {
@@ -15,10 +10,6 @@ namespace GW2NET.Accounts
     /// <summary>Provides the interface for repositories that provide player data.</summary>
     public interface IAccountRepository
     {
-        /// <summary>Gets the information about an account.</summary>
-        /// <returns>The <see cref="Account"/> for a given api key.</returns>
-        Account GetInformation();
-
         /// <summary>Asynchronously gets the information about an account.</summary>
         /// <returns>The <see cref="Account"/> for a given api key.</returns>
         Task<Account> GetInformationAsync();
