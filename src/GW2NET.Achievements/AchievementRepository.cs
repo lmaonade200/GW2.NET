a51d06475299bf13a1023c4d3f74d9316dbab8b0
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2016-03-09 00:43:03 +0100

    Update AchievementRepository documentation

--- src/GW2NET.Achievements/AchievementRepository.cs
+++ src/GW2NET.Achievements/AchievementRepository.cs
@@ -14,12 +14,16 @@ namespace GW2NET.Achievements
     using GW2NET.Common.Converters;
     using GW2NET.Common.Messages;
 
+    /// <summary>Provides access to to the achievements at the Guild Wars 2 api.</summary>
     public class AchievementRepository : CachedRepository<int, Achievement>, IDiscoverable<int>, ICachedRepository<int, AchievementDataModel, Achievement>, ILocalizable
     {
         /// <summary>Initializes a new instance of the <see cref="CachedRepository{TKey,TValue}"/> class.</summary>
         /// <param name="httpClient">The <see cref="HttpClient"/> to make connections with the GW2 api.</param>
         /// <param name="responseConverter">The <see cref="IResponseConverter"/> converting an <see cref="HttpResponseMessage"/> for further processing.</param>
         /// <param name="cache">The cache used to store objects.</param>
+        /// <param name="identifiersConverter">A converter used to convert identifiers.</param>
+        /// <param name="modelConverter">A converter used to convert data contracts into objects.</param>
+        /// <exception cref="ArgumentNullException">Thrown when either parameter is null.</exception>
         public AchievementRepository(HttpClient httpClient, IResponseConverter responseConverter, ICache<int, Achievement> cache, IConverter<int, int> identifiersConverter, IConverter<AchievementDataModel, Achievement> modelConverter)
             : base(httpClient, responseConverter, cache)
         {
