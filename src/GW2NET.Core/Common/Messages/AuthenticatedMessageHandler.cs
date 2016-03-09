b2ef4a355a7e9c0077f8f8b55c9664afdb737422
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2016-03-09 22:44:21 +0100

    Add authentification logic to AuthenticatedMessageHandler

--- src/GW2NET.Core/Common/Messages/AuthenticatedMessageHandler.cs
+++ src/GW2NET.Core/Common/Messages/AuthenticatedMessageHandler.cs
@@ -4,18 +4,37 @@
 
 namespace GW2NET.Common.Messages
 {
+    using System;
     using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading;
+    using System.Threading.Tasks;
 
+    /// <summary>Message handler to make authenticated requests against the Guild Wars 2 api.</summary>
     public class AuthenticatedMessageHandler : DelegatingHandler
     {
         private readonly string apiKey;
 
+        /// <summary>Initializes a new instance of the <see cref="AuthenticatedMessageHandler"/> class.</summary>
+        /// <param name="innerHandler">The inner handler used to make the actual request.</param>
+        /// <param name="apiKey">The api-key used to authenticate requests.</param>
         public AuthenticatedMessageHandler(HttpMessageHandler innerHandler, string apiKey)
         {
-            this.apiKey = KeyUtilities.IsValid(apiKey) ? apiKey : string.Empty;
+            if (!KeyUtilities.IsValid(apiKey))
+            {
+                throw new ArgumentException("The api-key did not have the correct format.");
+            }
+
+            this.apiKey = apiKey;
             this.InnerHandler = innerHandler;
         }
 
-        // ToDo: Implement Authentification
+        /// <inheritdoc />
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", this.apiKey);
+
+            return base.SendAsync(request, cancellationToken);
+        }
     }
 }
